74c3e9b54903af4820f874edbb378d8d12d5e72a
geekharb <73435240+geekharb@users.noreply.github.com>
2021-06-23 02:51:31 +0800

    test_site

---
+++ wp-login.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; Portfolio, by Feng Weihan &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="dashicons-css" href="http://geekharb.github.io/myportfolio/wp-includes/css/dashicons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://geekharb.github.io/myportfolio/wp-includes/css/buttons.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="forms-css" href="http://geekharb.github.io/myportfolio/wp-admin/css/forms.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://geekharb.github.io/myportfolio/wp-admin/css/l10n.min.css?ver=5.7.2" media="all">
+<link rel="stylesheet" id="login-css" href="http://geekharb.github.io/myportfolio/wp-admin/css/login.min.css?ver=5.7.2" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://geekharb.github.io/myportfolio/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://geekharb.github.io/myportfolio/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+								<a href="http://geekharb.github.io/myportfolio/wp-login.php?action=lostpassword">Lost your password?</a>
+			</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog">
+			<a href="http://geekharb.github.io/myportfolio/">&larr; Go to Portfolio, by Feng Weihan</a>		</p>
+			</div>
+	<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/jquery/jquery.min.js?ver=3.5.1" id="jquery-core-js"></script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"http:\/\/geekharb.github.io\/myportfolio\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script id="wp-polyfill-js-after">
+( 'fetch' in window ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );( 'objectFit' in document.documentElement.style ) || document.write( '<script defer='defer' src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/vendor/wp-polyfill-object-fit.min.js?ver=2.3.4"><\/scr' + 'ipt>' );
+</script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/hooks.min.js?ver=50e23bed88bcb9e6e14023e9961698c1" id="wp-hooks-js"></script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/dist/i18n.min.js?ver=db9a9a37da262883343e941c3731bc67" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/localsite\/wp-admin\/admin-ajax.php"}};
+</script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"569bc1cff1"};
+</script>
+<script id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", { "locale_data": { "messages": { "": {} } } } );
+</script>
+<script defer src="http://geekharb.github.io/myportfolio/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
+			<script>
+			/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+			</script>
+				<div class="clear"></div>
+	</body>
+	
+	
+<!--
+Performance optimized by W3 Total Cache. Learn more: https://www.boldgrid.com/w3-total-cache/
+
+Object Caching 9/12 objects using disk
+Page Caching using disk: enhanced (Requested URI is rejected) 
+Content Delivery Network via N/A
+Lazy Loading
+Minified using disk (Request URI is rejected)
+Database Caching using disk (Request-wide Request URI is rejected)
+
+Served from: localhost @ 2021-06-22 18:26:17 by W3 Total Cache
+-->
+</html>
